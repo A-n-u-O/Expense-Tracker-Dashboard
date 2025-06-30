5cea8ff9ab678ea1df2cdeefe4bc700c9ba7268b
Anuoluwapo <anuvictor2005@gmail.com>
2025-06-30 18:05:30 +0100

    expense summary added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { AddTransactionForm } from "@/components/AddTransactionForm";
+import { Summary } from "@/components/Summary";
 import { TransactionList } from "@/components/TransactionList";
 
 // import TestComponent from "./TestComponent";
 
 export default function Home() {
   return (
-    <main className=" flex min-h-screen items-center justify-center bg-blue-100">
+    <main className=" flex min-h-screen items-center justify-between p-8 bg-blue-100">
       <AddTransactionForm /> {/* <TestComponent /> */}
+      <Summary/>
       <TransactionList />
     </main>
   );
---
+++ src/components/Summary.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useTransactionStore } from "@/store/useTransactionStore";
+
+export const Summary = () => {
+  const transactions = useTransactionStore((state) => state.transactions);
+
+  const income = transactions
+    .filter((tx) => tx.type === "income")
+    .reduce((acc, tx) => acc + tx.amount, 0);
+
+  const expense = transactions
+    .filter((tx) => tx.type === "expense")
+    .reduce((acc, tx) => acc + tx.amount, 0);
+
+  const balance = income - expense;
+  return (
+    <div className=" bg-white p-6 rounded-lg shadow-md max-w-md w-full mt-6">
+      <h2 className=" text-lg font-semibold text-gray-800 mb-4">Summary</h2>
+      <div className=" flex justify-between mb-2">
+        <span className=" text-green-600 font-semibold"> Total Income:</span>
+        <span>₦{income.toLocaleString()}</span>
+      </div>
+      <div className=" flex justify-between mb-2">
+        <span className=" text-green-600 font-semibold"> Total Expense:</span>
+        <span>₦{expense.toLocaleString()}</span>
+      </div>
+      <div className=" flex justify-between mt-4 border-t pt-2">
+        <span> Balance:</span>
+        <span
+          className={`font-bold ${
+            balance >= 0 ? "text-green-700" : "text-red-700"
+          }`}>
+          ₦{balance.toLocaleString()}
+        </span>
+      </div>
+    </div>
+  );
+};
